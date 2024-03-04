d71aca4512c9f9482d20c4d8d42600a824d5f93d
JadenGuevara <116961723+JGuevara626@users.noreply.github.com>
2024-03-03 20:55:38 -0500

    Ship Actions WIP
    for demo recording progress

--- Assets/Scripts/Battleship.cs
+++ Assets/Scripts/Battleship.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,7 +13,7 @@ public class Battleship : MonoBehaviour
     // destroyed or healthy
     public bool destroyed;
     //the target of its choice
-    public double target;
+    public Vector2 target = new Vector2();
     // shield = true or false
     public bool shield;
     // speed of movement(maybe unnecessary if we just want to make the ship render in a different square rather than sliding over.)
@@ -20,7 +21,9 @@ public class Battleship : MonoBehaviour
     public Tile OccupiedTile;
     public int player = 0;
     public bool moving = false;
+    public bool radarScan = false;
     private List<Tile> tileMovement = new List<Tile>();
+    public bool lockChoice = false;
     // playerNumber( which player it belongs to)- I think we can just refer to its parent, and when we instantiate the ship, we can make it a child of the player
 
     // Functions
@@ -32,7 +35,7 @@ public class Battleship : MonoBehaviour
         //position = 0;
         destroyed = false;
         shield = false;
-        target = 0.0;
+        //target = new Vector2(;
         speed = 1; // we can decide how fast the ships will move
         choice = "unselected";
     }
@@ -40,41 +43,112 @@ public class Battleship : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (moving && Input.GetMouseButtonDown(0))
+        {
+            moving = false;
+            setNewPOS();
+        }
+    }
+
+    public void setNewPOS()
+    {
+        bool b = false;
+        var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 v2 = new Vector3(Mathf.RoundToInt(mouseWorldPos.x), Mathf.RoundToInt(mouseWorldPos.y));
+        foreach (Tile tile in tileMovement)
+        {
+            if (tile.transform.position == v2)
+            {
+                transform.position = v2;
+                OccupiedTile = tile;
+                b = true;
+                lockChoice = true;
+                CardsGroupHandler.instance.handleCards(false);
+                break;
+            }
+        }
+
+        if (!b)
+        {
+            moving = true;
+        }
+        else
+        {
+            foreach (Tile tile in tileMovement)
+            {
+                tile.SetHighlight(false);
+            }
+
+            tileMovement.Clear();
+            moving = false;
+        }
     }
 
     private void OnMouseDown()
     {
-        if(choice != "unselected")
+        if (!lockChoice)
         {
-            if (moving)
+            switch (choice)
             {
-                foreach (Tile tile in tileMovement)
-                {
-                    tile.SetHighlight(false);
-                }
-
-                tileMovement.Clear();
-                moving = false;
-
-            }
-            else
-            {
-                bool b = false;
-                Vector2 centerV = new Vector2(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
-                if (player == 1)
-                {
-                    b = true;
-                }
-
-                tileMovement = GridManager.instance.GetDirectionalTiles(centerV, b);
-                foreach (Tile tile in tileMovement)
-                {
-                    tile.SetHighlight(true);
-                }
-                moving = true;
+                case "Move":
+                    highlightSpaces();
+                    break;
+                case "Radar":
+                    usingRadar();
+                    break;
             }
         }
 
+    }
+
+    private void usingRadar()
+    {
+        if(player == 1)
+        {
+            GameManager.Instance.radarScanning(true);
+        }
+        else
+        {
+            GameManager.Instance.radarScanning(false);
+        }
+        
+        CardsGroupHandler.instance.handleCards(false);
+    }
+
+    public void resetChoice()
+    {
+        choice = "unselected";
+        lockChoice = true;
+    }
+
+    private void highlightSpaces()
+    {
+        if (moving)
+        {
+            foreach (Tile tile in tileMovement)
+            {
+                tile.SetHighlight(false);
+            }
+
+            tileMovement.Clear();
+            moving = false;
+
+        }
+        else
+        {
+            bool b = false;
+            Vector2 centerV = new Vector2(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
+            if (player == 1)
+            {
+                b = true;
+            }
+
+            tileMovement = GridManager.instance.GetDirectionalTiles(centerV, b);
+            foreach (Tile tile in tileMovement)
+            {
+                tile.SetHighlight(true);
+            }
+            moving = true;
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,12 @@
+using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Services.Matchmaker.Models;
 using UnityEngine;
+using static PlayerController;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks
 {
     //characteristics of game manager
     public static GameManager Instance;
@@ -21,8 +25,14 @@ public class GameManager : MonoBehaviour
     public PlayerController player1;//private?
     public PlayerController player2;//private?
     public GameObject bs;
+    public GameObject CardsGroupUI;
+    private bool searchingTile = false;
+    private string heldAction = "";
+    public List<Tile> radarships = new List<Tile>();
+    private List<GameObject> Scannedships = new List<GameObject>();
+    private Battleship tokenShip;
     //functions
-
+    ExitGames.Client.Photon.Hashtable playerProperty = new ExitGames.Client.Photon.Hashtable();
     // handle result of turn
     void HandleTurnOrder()
     {
@@ -36,8 +46,54 @@ public class GameManager : MonoBehaviour
 
         //reset states and timer
     }
-    void HandleShipAction(string action, double target)
+
+    public void intakeAction(string action)
     {
+        if (action != "")
+        {
+            searchingTile = true;
+        }
+        else
+        {
+            searchingTile = false;
+        }
+        heldAction = action;
+    }
+
+    void HandleShipAction()
+    {
+        //Battleship bs = new Battleship();
+        switch (PhotonNetwork.LocalPlayer.ActorNumber)
+        {
+            case 1:
+                tokenShip = player1.getShip();
+                break;
+            case 2:
+                tokenShip = player2.getShip();
+                break;
+        }
+
+        bool b = false;
+        switch(heldAction)
+        {
+            case "Move":
+                tokenShip.choice = "Move";
+                b = true;
+                break;
+            case "Radar":
+                tokenShip.choice = "Radar";
+                b = true;
+                if (PhotonNetwork.LocalPlayer.ActorNumber == 1)
+                {
+                    GameManager.Instance.radarScanning(true);
+                }
+                else
+                {
+                    GameManager.Instance.radarScanning(false);
+                }
+                    break;
+        }
+
         // if ship is not alive, just skip 
         // if ship action is shoot, shootCannon(pl)
         // if ship action is move, moveShip()
@@ -45,8 +101,55 @@ public class GameManager : MonoBehaviour
         // if ship action is default, shoot a cannonball at a random square?
 
         // wipe ship's selection clean
+        if (!b)
+        {
+            searchingTile = true;
+        }
     }
 
+    public void radarScanning(bool isPlayer1)
+    {
+        callShipLocation();
+        PlayerController pc = gameObject.AddComponent<PlayerController>();
+        clearScan();
+        if (isPlayer1)
+        {
+            foreach( Battleship bs in player2.shipList)
+            {
+                radarships.Add(bs.OccupiedTile);
+            }
+        }
+        else
+        {
+            foreach (Battleship bs in player1.shipList)
+            {
+                radarships.Add(bs.OccupiedTile);
+            }
+        }
+
+        foreach (Tile t in radarships)
+        {
+            GameObject go = Instantiate(bs, t.transform);
+            go.GetComponent<SpriteRenderer>().color = Color.green;
+            Scannedships.Add(go);
+        }
+        
+
+    }
+
+    void clearScan()
+    {
+        foreach (Tile t in radarships)
+        {
+            Destroy(t.gameObject);
+        }
+        radarships.Clear();
+        foreach (GameObject go in Scannedships)
+        {
+            DestroyImmediate(go);
+        }
+        Scannedships.Clear();
+    }
 
     //shootCannon(shotFrom, shotTo)
     void shootCannon()
@@ -54,7 +157,7 @@ public class GameManager : MonoBehaviour
         // spawn a cannonball at shootFrom and make its target shotTo
     }
     //moveShip(moveTo)'
-    void moveShip()// figure out what data type a ship's position is
+    void moveShip(Battleship bs)// figure out what data type a ship's position is
     {
         // position = newPosition;
     }
@@ -73,6 +176,7 @@ public class GameManager : MonoBehaviour
         player2.player = 2;
         player2.shipObj = bs;
         ChangeState(gameState.shipPlacement);
+        print(photonView);
     }
 
     public void ChangeState(gameState gstate) 
@@ -92,6 +196,45 @@ public class GameManager : MonoBehaviour
                 break;
         }
     }
+
+    [PunRPC]
+    public void UpdatePlayerController(PlayerState pc, int n)
+    {
+        switch (n)
+        {
+            case 1:
+                player1.status = pc;
+
+                break;
+            case 2:
+                player2.status = pc;
+                break;
+        }
+
+        if(player1.status == PlayerState.placementReady && player2.status == PlayerState.placementReady)
+        {
+            CardsGroupUI.SetActive(true);
+            player1.ChangeState(PlayerState.selectActions);
+            player2.ChangeState(PlayerState.selectActions);
+        }
+
+    }
+
+    [PunRPC]
+    public void updateShipCoords(int n, List<Battleship> shipList)
+    {
+        switch (n)
+        {
+            case 1:
+                player1.shipList = shipList;
+                break;
+            case 2:
+                player2.shipList = shipList;
+                break;
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
@@ -101,5 +244,67 @@ public class GameManager : MonoBehaviour
         //    HandleTurnOrder();
         //}
 
+        if(searchingTile && Input.GetMouseButtonDown(0))
+        {
+            searchingTile = false;
+            HandleShipAction();
+        }
+
     }
+
+    public void sendPC(int n)
+    {
+
+        if (n == 1)
+        {
+            print("if player1 is true:" + player1.isActiveAndEnabled);
+            photonView.RPC("UpdatePlayerController", RpcTarget.All, player1.status, n);
+        }
+        else if (n == 2)
+        {
+            photonView.RPC("UpdatePlayerController", RpcTarget.All, player2.status, n);
+        }
+        
+    }
+
+    [PunRPC]
+    public void sendShips()
+    {
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 1)
+        {
+            photonView.RPC("updateShipCoords", RpcTarget.All, 1, player1.shipList);
+        }
+        else if (PhotonNetwork.LocalPlayer.ActorNumber == 2)
+        {
+            photonView.RPC("updateShipCoords", RpcTarget.All, 2,player2.shipList);
+        }
+    }
+
+    public void callShipLocation()
+    {
+        photonView.RPC("sendShips", RpcTarget.All);
+    }
+
+
+    //public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    //{
+    //    if (PhotonNetwork.LocalPlayer == targetPlayer)
+    //    {
+    //        updateClient(targetPlayer);
+    //    }
+    //}
+
+    //public void updateClient(Player targetPlayer)
+    //{
+    //    if (targetPlayer.CustomProperties.ContainsKey("pc1"))
+    //    {
+    //        player1 = (PlayerController)targetPlayer.CustomProperties["pc1"];
+    //        playerProperty["pc1"] = (PlayerController)targetPlayer.CustomProperties["pc1"];
+    //    }
+    //    else if (targetPlayer.CustomProperties.ContainsKey("pc2"))
+    //    {
+    //        player2 = (PlayerController)targetPlayer.CustomProperties["pc2"];
+    //        playerProperty["pc2"] = (PlayerController)targetPlayer.CustomProperties["pc2"];
+    //    }
+    //}
 }
